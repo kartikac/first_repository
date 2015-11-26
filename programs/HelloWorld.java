12980dd649c3668a9118eb8a7c50b44a6d4b741a
Kartik Chandrasekhar <kartikac@gmail.com>
2015-11-25 21:00:48 -0800

    checking in modified HelloWorld.java file

--- programs/HelloWorld.java
+++ programs/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld { 
    public static void main(String[] args) { 
-      System.out.println("Hello, World this is a test");
+      System.out.println("Hello, World this is a test in git");
    }
 }
