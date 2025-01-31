9afadfad9fa9241ab0c2c58d52a91cf9bf53fee3
kartikac <kartikac@gmail.com>
2025-01-31 11:59:23 +0530

    Updated main.c

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World! \n");
-   printf("This is push request! \n");
+   printf("This is push request!! \n");
    return 0;
 }
