da7f1099278adfecdac1874af72892e0d30a357b
kartikac <kartikac@gmail.com>
2025-02-03 23:01:28 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World! \n");
-   printf("This is web hook pushing the build 4 !! \n");
+   printf("This is web hook pushing the build 5 !! \n");
    return 0;
 }
